7ae5ee97a7fc6c3b988c1481086501c2b10851be
MOLO MOLO PACIFIQUE <molo2206@users.noreply.github.com>
2025-04-18 10:16:20 +0200

    username

--- db/data-source.ts
+++ db/data-source.ts
@@ -4,16 +4,11 @@ import { config } from 'dotenv';
 config();
 export const dataSourceOptions: DataSourceOptions = {
     type: 'mysql',
-    // host: '127.0.0.1', // ✅ Remplace localhost par cette adresse
-    // port: 3306,
-    // username: 'root',
-    // password: '',
-    // database: 'favor_db',
-    host: process.env.DB_HOST,
+    host: 'localhost', // ✅ Remplace localhost par cette adresse
     port: 3306,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    username: 'admin',
+    password: 'favor@2025',
+    database: 'favor_bd',
     entities: ['dist/**/*.entity{.ts,.js}'],
     migrations: ['dist/db/migrations/*{.ts,.js}'],
     logging: false,
