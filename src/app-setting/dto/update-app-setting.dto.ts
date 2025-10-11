d85ceb4d06a5bca2b474a4e8fe9804126123afd1
Molo Pacifique <molomoloisraelpacifique@gmail.com>
2025-10-11 11:20:10 +0200

    service

--- src/app-setting/dto/update-app-setting.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateAppSettingDto } from './create-app-setting.dto';
-
-export class UpdateAppSettingDto extends PartialType(CreateAppSettingDto) {}
