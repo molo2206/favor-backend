80e13bde8ccaf9d58527d590b663004c521b9e34
Molo Pacifique <molomoloisraelpacifique@gmail.com>
2025-10-22 14:38:30 +0200

    sms

--- src/otp/dto/reset-password.dto.ts
+++ src/otp/dto/reset-password.dto.ts
@@ -1,18 +1,35 @@
-// reset-password.dto.ts
+import { IsNotEmpty, IsString, Matches, Validate } from 'class-validator';
+import {
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+  ValidationArguments,
+} from 'class-validator';
+import * as validator from 'validator';
 
-import { IsEmail, IsNotEmpty, Matches } from 'class-validator';
+@ValidatorConstraint({ name: 'isEmailOrPhone', async: false })
+export class IsEmailOrPhoneConstraint implements ValidatorConstraintInterface {
+  validate(value: string, args: ValidationArguments) {
+    return validator.isEmail(value) || validator.isMobilePhone(value, 'any');
+  }
+
+  defaultMessage(args: ValidationArguments) {
+    return 'Le champ doit être une adresse e-mail valide ou un numéro de téléphone valide';
+  }
+}
 
 export class ResetPasswordDto {
-    @IsNotEmpty({ message: 'Email cannot be null' })
-    @IsEmail({}, { message: 'Email must be a valid email address' })
-    email: string;
+  @IsNotEmpty({ message: 'Veuillez fournir un email ou un numéro de téléphone' })
+  @IsString()
+  @Validate(IsEmailOrPhoneConstraint)
+  email: string; // reste email pour la compatibilité existante
 
-    @IsNotEmpty({ message: "Le code OTP est requis." })
-    otpCode: string;
+  @IsNotEmpty({ message: 'Le code OTP est requis.' })
+  otpCode: string;
 
-    @IsNotEmpty({ message: "Le mot de passe est requis." })
-    @Matches(/(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])/, {
-        message: 'Le mot de passe doit contenir au moins une majuscule, une minuscule et un chiffre.',
-    })
-    password: string;
+  @IsNotEmpty({ message: 'Le mot de passe est requis.' })
+  @Matches(/(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])/, {
+    message:
+      'Le mot de passe doit contenir au moins une majuscule, une minuscule et un chiffre.',
+  })
+  password: string;
 }
