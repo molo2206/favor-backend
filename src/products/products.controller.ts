a01c41ea81d5c69cfb2bf3b7c8b967da6bf5bab8
Molo Pacifique <molomoloisraelpacifique@gmail.com>
2025-10-22 15:00:44 +0200

    sms

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -47,7 +47,7 @@ export class ProductController {
     @Body() body: any,
     @CurrentUser() user: UserEntity,
   ) {
-    if (!files || files.length < 2 || files.length > 4) {
+    if (!files) {
       throw new BadRequestException("Le nombre d'images doit être compris entre 2 et 4");
     }
 
