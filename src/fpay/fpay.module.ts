b5e6bd9644357bbbb15767f047c0b918949a704d
molo2206 <molomoloisraelpacifique@gmail.com>
2026-08-26 15:53:26 +0200

    correction link

--- src/fpay/fpay.module.ts
+++ src/fpay/fpay.module.ts
@@ -1,8 +1,10 @@
 // src/modules/fpay/fpay.module.ts
+
 import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt'; // ✅ Garder l'import
 import { FpayService } from './fpay.service';
 import { FpayController } from './fpay.controller';
 import { UserEntity } from 'src/users/entities/user.entity';
@@ -15,6 +17,7 @@ import { UserEntity } from 'src/users/entities/user.entity';
         }),
         ConfigModule,
         TypeOrmModule.forFeature([UserEntity]),
+        JwtModule, // ✅ Juste JwtModule sans configuration
     ],
     controllers: [FpayController],
     providers: [FpayService],
